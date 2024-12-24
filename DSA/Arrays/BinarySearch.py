6b0f9d4f9f670dd7aee2dc5e3f1de5f9cba13cd2
Benjamin_C <benjamin_c@qq.com>
2024-12-24 21:55:05 +0800

    update binarySearch with some functions

--- DSA/Arrays/BinarySearch.py
+++ DSA/Arrays/BinarySearch.py
@@ -10,9 +10,14 @@ def binarySearch(arrays, targetVal):
         else:
             right = mid -1
     return -1
-
+#For the Binary Search algorithm to work,the array must already be sorted, so we can use Sort Alogrithm first when we face some unsorted arrays
 if __name__ == '__main__':
-    arr = [1,5,12,34,55,66,67,85,87,101]
+    from QuickSort import QuickSort
+
+    arr1 = [1,5,12,34,55,66,67,85,87,101]
+    arr2 = [12,1,5,34,66,55,85,87,101,67]
+    arr = QuickSort(arr1)
+    #arr = QuickSort(arr2)    #But this way can not find the elements' index in original arrays
     n = 101
     if binarySearch(arr, n) != -1:
         print(f'Found {n} at index - {binarySearch(arr ,n)} -')
